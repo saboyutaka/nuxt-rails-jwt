cb7466a5b5c73d7c0c791901ff79005a45243b01
saboyutaka <yutaka.paradigm.shift@gmail.com>
2020-05-12 21:53:26 +0900

    :+1: signupを実装

--- rails/app/controllers/authentication_controller.rb
+++ rails/app/controllers/authentication_controller.rb
@@ -1,5 +1,5 @@
 class AuthenticationController < ApplicationController
-  before_action :authorize_request, except: :login
+  before_action :authorize_request, except: [:signup, :login]
 
   # POST /auth/login
   def login
@@ -13,9 +13,27 @@ class AuthenticationController < ApplicationController
     end
   end
 
+  def signup
+    @user = User.new(signup_params)
+    if @user.save
+      token = JsonWebToken.encode(user_id: @user.id)
+      time = Time.now + 24.hours.to_i
+      render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"), email: params[:email]}, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages },
+             status: :unprocessable_entity
+    end
+  end
+
   private
 
   def login_params
     params.permit(:email, :password)
+  end
+
+  def signup_params
+    params.permit(
+      :name, :email, :password, :password_confirmation
+    )
   end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post '/signup', to: 'authentication#signup'
   post '/auth/login', to: 'authentication#login'
 end

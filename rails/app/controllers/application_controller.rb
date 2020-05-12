5b94cc637b44d61054bad7e3d7dccbc608cec844
saboyutaka <yutaka.paradigm.shift@gmail.com>
2020-05-12 22:31:00 +0900

    :+1: errorメッセージを変更

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::API
   def authorize_request
     header = request.headers['Authorization']
-    header = header.split(' ').last if header
+    if header
+      token = header.split(' ').last
+    else
+      return render json: { errors: 'Authorization is not given.' }, status: :unauthorized
+    end
+
     begin
-      @decoded = JsonWebToken.decode(header)
+      @decoded = JsonWebToken.decode(token)
       @current_user = User.find(@decoded[:user_id])
     rescue ActiveRecord::RecordNotFound => e
       render json: { errors: e.message }, status: :unauthorized

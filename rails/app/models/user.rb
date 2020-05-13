4d16c5622ffb5319312774730f8d0a83adfcd85f
saboyutaka <yutaka.paradigm.shift@gmail.com>
2020-05-13 22:36:02 +0900

    👍 validationを追加

--- rails/app/models/user.rb
+++ rails/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_secure_password
+  validates :name, presence: true
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password,

15b8da94156d168b9b0966b35b52a5c9d6f38d4a
saboyutaka <yutaka.paradigm.shift@gmail.com>
2020-05-13 23:28:59 +0900

    :+1: CORSにput, patch, delete も追加

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -37,7 +37,7 @@ module App
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins ENV['CORS_ORIGIN']
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
       end
     end
 

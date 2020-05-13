a2e19ddb31a681d766f834d8ac82f59af26e981e
saboyutaka <yutaka.paradigm.shift@gmail.com>
2020-05-13 22:29:54 +0900

    👍 CORS

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -33,5 +33,13 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins ENV['CORS_ORIGIN']
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
+
   end
 end
